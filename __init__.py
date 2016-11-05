d052255bf83f7953ac44e8c3b1e03284f101779e
Jeiel Aranal <jeiel.aranal@gmail.com>
2016-11-05 16:06:59 +0800

    Removed scene datatypes that were moved to SprytileSceneSettings

--- __init__.py
+++ __init__.py
@@ -18,12 +18,14 @@ import bpy
 from bpy.props import *
 
 class SprytileSceneSettings(bpy.types.PropertyGroup):
+
     def set_normal(self, value):
-        if self.locknormal is True:
+        if self.lock_normal is True:
             return
-        self["sprytile_normalmode"] = value
+        self["normal_mode"] = value
+
     def get_normal(self):
-        return self["sprytile_normalmode"]
+        return self["normal_mode"]
 
     normal_mode = EnumProperty(
         items = [
@@ -38,13 +40,13 @@ class SprytileSceneSettings(bpy.types.PropertyGroup):
         get = get_normal
     )
 
-    locknormal = BoolProperty(
+    lock_normal = BoolProperty(
         name = "Lock",
         description = "Lock normal used to create meshes",
         default = False
     )
 
-    paintmode = EnumProperty(
+    paint_mode = EnumProperty(
         items = [
             ("PAINT",       "Paint",        "", 1),
             ("MAKE_FACE",   "Build",        "Only create new faces", 3),
@@ -64,13 +66,13 @@ class SprytileSceneSettings(bpy.types.PropertyGroup):
         max = 2048
     )
 
-    paint_normalve = FloatVectorProperty(
+    paint_normal_vector = FloatVectorProperty(
         name = "Srpytile Last Paint Normal",
         description = "Last saved painting normal used by Sprytile",
         subtype = 'DIRECTION',
         default = (0.0, 0.0, 1.0)
     )
-    paint_upvector = FloatVectorProperty(
+    paint_up_vector = FloatVectorProperty(
         name = "Sprytile Last Paint Up Vector",
         description = "Last saved painting up vector used by Srpytile",
         subtype = 'DIRECTION',
@@ -119,59 +121,14 @@ def get_normal(self):
 
 def setup_props():
     bpy.types.Scene.sprytile_data = bpy.props.PointerProperty(type=SprytileSceneSettings)
-    bpy.types.Material.sprytile_grids = bpy.props.CollectionProperty(type=SprytileMaterialGridSettings)
-    # Scene properties
-    bpy.types.Scene.sprytile_normalmode = EnumProperty(
-        items = [
-            ("X",           "X",    "World X-Axis",     1),
-            ("Y",           "Y",    "World Y-Axis",     2),
-            ("Z",           "Z",    "World X-Axis",     3)
-        ],
-        name = "Normal Mode",
-        description = "Normal to create the mesh on",
-        default = 'Z',
-        set = set_normal,
-        get = get_normal
-    )
-    bpy.types.Scene.sprytile_locknormal = BoolProperty(
-        name = "Lock",
-        description = "Lock normal used to create meshes",
-        default = False
-    )
-
-    bpy.types.Scene.sprytile_paintmode = EnumProperty(
-        items = [
-            ("PAINT",       "Paint",        "", 1),
-            ("MAKE_FACE",   "Build",        "Only create new faces", 3),
-            ("SET_NORMAL",  "Set Normal",   "Select a normal to use for face creation", 2)
-        ],
-        name = "Sprytile Paint Mode",
-        description = "Paint mode",
-        default = 'PAINT'
-    )
-    bpy.types.Scene.sprytile_normal_data = FloatVectorProperty(
-        name = "Srpytile Last Paint Normal",
-        description = "Last saved painting normal used by Sprytile",
-        default = (0.0, 0.0, 1.0)
-    )
-    bpy.types.Scene.sprytile_upvector_data = FloatVectorProperty(
-        name = "Sprytile Last Paint Up Vector",
-        description = "Last saved painting up vector used by Srpytile",
-        default = (0.0, 1.0, 0.0)
-    )
-    bpy.types.Scene.sprytile_world_pixels = IntProperty(
-        name = "World Pixel Density",
-        description = "How many pixels are displayed in one world unit",
-        default = 32,
-        min = 8,
-        max = 2048
-    )
 
     # Object properties
     bpy.types.Object.sprytile_matid = IntProperty(
         name = "Material ID",
         description = "Material index used for object"
     )
+
+    bpy.types.Material.sprytile_grids = bpy.props.CollectionProperty(type=SprytileMaterialGridSettings)
     bpy.types.Material.sprytile_mat_grid_x = IntProperty(
         name = "Width",
         description = "Texture grid width, in pixels",
@@ -190,14 +147,7 @@ def setup_props():
 def teardown_props():
     del bpy.types.Scene.sprytile_data
     del bpy.types.Material.sprytile_grids
-
-    del bpy.types.Scene.sprytile_normalmode
-    del bpy.types.Scene.sprytile_locknormal
-    del bpy.types.Scene.sprytile_paintmode
-    del bpy.types.Scene.sprytile_normal_data
-    del bpy.types.Scene.sprytile_upvector_data
-    del bpy.types.Scene.sprytile_world_pixels
-
+    
     del bpy.types.Object.sprytile_matid
     del bpy.types.Material.sprytile_mat_grid_x
     del bpy.types.Material.sprytile_mat_grid_y
