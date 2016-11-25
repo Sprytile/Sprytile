f00e7268d2febde80acb6d873067306a31f87870
Jeiel Aranal <jeiel.aranal@gmail.com>
2016-11-26 02:03:58 +0800

    Added readout to pixel grid translate

--- sprytile_utils.py
+++ sprytile_utils.py
@@ -1,4 +1,8 @@
 import bpy
+import bgl
+import blf
+import bmesh
+from bmesh.types import BMVert, BMEdge, BMFace
 from mathutils import Matrix, Vector
 from . import sprytile_modal
 
@@ -263,15 +267,52 @@ class SprytileGridTranslate(bpy.types.Operator):
     bl_idname = "sprytile.translate_grid"
     bl_label = "Sprytile Pixel Translate"
 
+    @staticmethod
+    def draw_callback(self, context):
+        if self.exec_counter != -1 or self.ref_pos is None:
+            return None
+
+        check_pos = self.get_ref_pos(context)
+        measure_vec = check_pos - self.ref_pos
+        pixel_unit = 1 / context.scene.sprytile_data.world_pixels
+        for i in range(3):
+            measure_vec[i] = int(round(measure_vec[i] / pixel_unit))
+
+        screen_y = context.region.height - 45
+        screen_x = 20
+        padding = 5
+
+        font_id = 0
+        font_size = 16
+        blf.size(font_id, font_size, 72)
+
+        bgl.glColor4f(1, 1, 1, 1)
+        # blf.enable(font_id, blf.SHADOW)
+        # blf.shadow(font_id, 0, 0, 0, 0, 1)
+        # blf.shadow_offset(font_id, 2, -2)
+
+        readout_axis = ['X', 'Y', 'Z']
+        for i in range(3):
+            blf.position(font_id, screen_x, screen_y, 0)
+            blf.draw(font_id, "%s : %d" % (readout_axis[i], measure_vec[i]))
+            screen_y -= font_size + padding
+
+        # blf.disable(font_id, blf.SHADOW)
+
     def modal(self, context, event):
         # User cancelled transform
+        if event.type == 'ESC':
+            return self.exit_modal(context)
         if event.type == 'RIGHTMOUSE' and event.value == 'RELEASE':
             return self.exit_modal(context)
         # On the timer events, count down the frames and execute the
         # translate operator when reach 0
-        if event.type == 'TIMER' and self.exec_counter > 0:
-            self.exec_counter -= 1
+        if event.type == 'TIMER':
+            if self.exec_counter > 0:
+                self.exec_counter -= 1
+
             if self.exec_counter == 0:
+                self.exec_counter -= 1
                 up_vec, right_vec, norm_vec = sprytile_modal.get_current_grid_vectors(context.scene)
                 norm_vec = sprytile_modal.snap_vector_to_axis(norm_vec)
                 axis_constraint = [
@@ -294,6 +335,19 @@ class SprytileGridTranslate(bpy.types.Operator):
 
         return {'PASS_THROUGH'}
 
+    def get_ref_pos(self, context):
+        if self.bmesh is None:
+            self.bmesh = bmesh.from_edit_mesh(context.object.data)
+
+        target = self.bmesh.select_history.active
+        if isinstance(target, BMFace):
+            return target.calc_center_median()
+        if isinstance(target, BMVert):
+            return target.co
+        if isinstance(target, BMEdge):
+            return target.verts[0].co
+        return None
+
     def execute(self, context):
         return self.invoke(context, None)
 
@@ -308,6 +362,7 @@ class SprytileGridTranslate(bpy.types.Operator):
                 "grid_sub": space_data.grid_subdivisions,
                 "show_floor": space_data.show_floor,
                 "pivot": context.space_data.pivot_point,
+                "orient": context.space_data.transform_orientation,
                 "snap_element": context.scene.tool_settings.snap_element
             }
             pixel_unit = 1 / context.scene.sprytile_data.world_pixels
@@ -315,6 +370,7 @@ class SprytileGridTranslate(bpy.types.Operator):
             space_data.grid_subdivisions = 1
             space_data.show_floor = False
             space_data.pivot_point = 'CURSOR'
+            space_data.transform_orientation = 'GLOBAL'
             context.scene.tool_settings.snap_element = 'INCREMENT'
         # Remember what the current active operator is, when it changes
         # we know that the translate operator is complete
@@ -324,6 +380,13 @@ class SprytileGridTranslate(bpy.types.Operator):
         # the translate tool will not use the new grid scale if we switch
         # over immediately to translate.
         self.exec_counter = 2
+
+        # Save the bmesh, and reference position
+        self.bmesh = None
+        self.ref_pos = self.get_ref_pos(context)
+
+        args = self, context
+        self.draw_handle = bpy.types.SpaceView3D.draw_handler_add(self.draw_callback, args, 'WINDOW', 'POST_PIXEL')
 
         win_mgr = context.window_manager
         self.timer = win_mgr.event_timer_add(0.1, context.window)
@@ -338,6 +401,7 @@ class SprytileGridTranslate(bpy.types.Operator):
             context.space_data.grid_subdivisions = self.restore_settings['grid_sub']
             context.space_data.show_floor = self.restore_settings['show_floor']
             context.space_data.pivot_point = self.restore_settings['pivot']
+            context.space_data.transform_orientation = self.restore_settings['orient']
             context.scene.tool_settings.snap_element = self.restore_settings['snap_element']
         # Didn't snap to grid, force to grid by calculating what the snapped translate would be
         else:
@@ -353,6 +417,8 @@ class SprytileGridTranslate(bpy.types.Operator):
                 offset = translation - op.properties.value
                 bpy.ops.transform.translate(value=offset)
 
+        self.bmesh = None
+        bpy.types.SpaceView3D.draw_handler_remove(self.draw_handle, 'WINDOW')
         context.window_manager.event_timer_remove(self.timer)
         return {'FINISHED'}
 
