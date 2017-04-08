230c618865b0c26c3d660041167528b369d7160f
Jeiel Aranal <jeiel.aranal@gmail.com>
2017-04-08 17:07:14 +0800

    Version bump

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Sprytile Painter",
     "author": "Jeiel Aranal",
-    "version": (0, 2, 5),
+    "version": (0, 2, 6),
     "blender": (2, 7, 0),
     "description": "A utility for creating tile based low spec scenes with paint/map editor tools",
     "location": "View3D > UI panel > Sprytile",
